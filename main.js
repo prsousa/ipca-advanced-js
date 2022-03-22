93ee6ce319ebc5b81880bc5861c5b1eefc3c4f42
Paulo Sousa <pcsousa@ipca.pt>
2022-03-22 22:57:08 +0000

    Challenge 2.2: use await instead of .then()

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
 import { getBorders, getCountry } from "./countries.js";
 
-const countryPromise = getCountry("PRT");
-
-countryPromise
-  .then((country) => {
-    const borders = getBorders(country);
-    console.log(borders);
-  })
-  .catch((err) => console.error("An error has occurred"));
+const country = await getCountry("PRT");
+const borders = getBorders(country);
+console.log(borders);
